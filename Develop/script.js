50f0a5702101a4859bbf2e1fef1a302ed6890fa0
yomiodusi <99558274+yomiodusi@users.noreply.github.com>
2022-03-13 17:45:46 -0400

    added variables for password

--- Develop/script.js
+++ Develop/script.js
@@ -1,19 +1,38 @@
+var actualPassword = 8;
+var possiblePassword = [];
+
+var specialCharOptions = [];
+var numberOptions = ['0','1','2','3','4','5','6','7','8','9'];
+var upperCaseOptions = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+var lowerCaseOptions = [];
+
 // Assignment code here
 function generatePassword() {
   console.log("You Clicked")
 
-  var passLength=prompt("How many characters would you like your password to contain. Must be between 8 to 28");
+  // 1. Prompt the user for the password criteria
+
+  var passLength=prompt("How many characters would you like your password to contain. Must be between 8 to 128");
   if (passLength < 8 || passLength > 128) {
-    window.alert("Please try again. Password Length must contain numbers from 8 to 128")
+    window.alert("Please try again. Password Length must be between 8 to 128 characters")
+    generatePassword();
   }
 
-  var special=prompt("Do you want to include special characters");
-  var upperCase=prompt("Do you want to include uppercase characters");
-  var lowerCase=prompt("Do you want to include lowercase characters");
+  
+  confirm ("Do you want to include special characters")
+
+  confirm ("Do you want to include numbers")
+
+  confirm ("Do you want to include uppercase characters")
+
+  confirm ("Do you want to include lowercase characters")
+  
+  var upperCase
+  var lowerCase
 
   // 1. Prompt the user for the password criteria
-  //  a. Password Length between 8 < 28
-  //  b. lowercase, uppercase, numberss and special characters
+
+  //  b. lowercase, uppercase, numbers and special characters
   // 2. Validate the input
   // 3. Generate Password Based on Criteria
   
