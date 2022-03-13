6d88cc57c16afde0a62d2cfd34860c7836b4bbb4
yomiodusi <99558274+yomiodusi@users.noreply.github.com>
2022-03-13 19:32:29 -0400

    working on writing function now

--- Develop/script.js
+++ Develop/script.js
@@ -1,47 +1,18 @@
-var actualPassword = 8;
+// Assignment code here
+
+var passwordLength = 5;
 var possiblePassword = [];
 
-var specialCharOptions = [];
+var specialCharOptions = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '+', '-', '.', '~', '|', '<', '>', '=', '-', '_', '/', ':', ';', '?', '[', ']', '{', '}', '~'];
 var numberOptions = ['0','1','2','3','4','5','6','7','8','9'];
-var upperCaseOptions = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-var lowerCaseOptions = [];
-
-// Assignment code here
-function generatePassword() {
-  console.log("You Clicked")
-
-  // 1. Prompt the user for the password criteria
-
-  var passLength=prompt("How many characters would you like your password to contain. Must be between 8 to 128");
-  if (passLength < 8 || passLength > 128) {
-    window.alert("Please try again. Password Length must be between 8 to 128 characters")
-    generatePassword();
-  }
-
-  
-  confirm ("Do you want to include special characters")
-
-  confirm ("Do you want to include numbers")
-
-  confirm ("Do you want to include uppercase characters")
-
-  confirm ("Do you want to include lowercase characters")
-  
-  var upperCase
-  var lowerCase
-
-  // 1. Prompt the user for the password criteria
-
-  //  b. lowercase, uppercase, numbers and special characters
-  // 2. Validate the input
-  // 3. Generate Password Based on Criteria
-  
-  // 4. Display Password to the page.
-  return "Generated Password will go here!"
-}
+var lowerCaseOptions = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+var upperCaseOptions = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',];
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
 
 // Write password to the #password input
 function writePassword() {
@@ -52,5 +23,42 @@ function writePassword() {
 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+function generatePassword() {
+  // 1. Prompt the user for the password criteria
+
+  possiblePassword = [];
+
+  var passwordLength = parseInt(prompt("How many characters would you like your password to contain. Must be between 8 to 128"));
+  if (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {
+    window.alert("Please try again. Password Length must be between 8 to 128 characters")
+    return false;
+  }
+
+  if (confirm("Do you want to include special characters")) {
+    possiblePassword = possiblePassword.concat(specialCharOptions);
+  }
+
+  if (confirm("Do you want to include include numbers")) {
+    possiblePassword = possiblePassword.concat(numberOptions);
+  }
+
+  if (confirm("Do you want to include uppercase character")) {
+    possiblePassword = possiblePassword.concat(upperCaseOptions);
+  }
+
+  if (confirm("Do you want to include lowercase characters")) {
+    possiblePassword = possiblePassword.concat(lowerCaseOptions);
+  }
+  console.log("You Clicked")
+
+  return true;
+}
+
+
+
+
+
+
+
+
+
